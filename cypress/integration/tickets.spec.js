608da097edb43850f11e0a0399746a73ef0ceb16
carolinacmiranda <costacarol87@gmail.com>
2021-05-05 15:01:43 -0300

    Create tests using Cypress

---
+++ cypress/integration/tickets.spec.js
@@ -0,0 +1,113 @@
+describe("Tickets", () => {
+
+    beforeEach(() => cy.visit("https://bit.ly/3tlNeha"));
+
+    it("fills all the text input fields", () => {
+
+        const firstName = "Carolina";
+        const lastName = "Costa Miranda";
+
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("myemail@email.com");
+        cy.get("#requests").type("QA");
+        cy.get("#signature").type(`${firstName} ${lastName}`);
+
+    });
+
+    it("select two tickets", () => {
+
+        cy.get("#ticket-quantity").select("2");
+
+    });
+
+    it("select 'vip' ticket type", () => {
+
+        cy.get("#vip").check();
+
+    });
+
+    it("selects 'social media' checkbox", () => {
+
+        cy.get("#social-media").check();
+        cy.get("#friend").click();
+        cy.get("#publication").check();
+        cy.get("#publication").uncheck();
+
+    });
+
+    it("has 'TICKETBOX' header's heading", () => {
+
+        cy.get("header h1").should("contain", "TICKETBOX");
+
+    });
+
+    it("alerts on invalid email", () => {
+
+        cy.get("#email")    
+          .as("email")
+          .type("invalidEmail-email.com");
+
+        cy.get("#email.invalid").should("exist");
+
+        cy.get("@email")
+          .clear()
+          .type("validEmail@email.com");
+
+        cy.get("#email.invalid").should("not.exist");
+
+    });
+
+    it("fills and reset the form", () => {
+
+        const firstName = "Carolina";
+        const lastName = "Costa Miranda";
+        const fullName = `${firstName} ${lastName}`;
+
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("myemail@email.com");
+        cy.get("#ticket-quantity").select("2");
+        cy.get("#vip").check();
+        cy.get("#friend").check();
+        cy.get("#requests").type("QA");
+
+        cy.get(".agreement p").should(
+            "contain",
+            `I, ${fullName}, wish to buy 2 VIP tickets.`
+        );
+
+        cy.get("#agree").click();
+        cy.get("#signature").type(fullName);
+        
+        cy.get("button[type='submit']")
+          .as("submitButton")
+          .should("not.be.disabled");
+
+        cy.get("button[type='reset']").click();
+
+        cy.get("@submitButton").should("be.disabled");
+
+    });
+
+    it("fills mandatory fields using support command", () => {
+
+        const customer = {
+            firstName: "João",
+            lastName: "Silva",
+            email: "joaosilva@email.com"
+        }
+
+        cy.fillsMandatoryFields(customer);
+
+        cy.get("button[type='submit']")
+          .as("submitButton")
+          .should("not.be.disabled");
+
+        cy.get("#agree").uncheck();
+
+        cy.get("@submitButton").should("be.disabled");
+
+    });
+
+});
